d21d5f469076e3f1e0c278660524ac9292bcc5ba
Levko Misha <misha_levko96@gmail.com>
2020-07-14 11:57:41 +0300

    finish header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
-console.log('asdsad');
+$(function () {
+	// icon-search
+	$("#search").click(function () {
+		$(".menu-item").addClass('hide-item');
+		$(".header__search-form").addClass('active');
+		$(".close").addClass('active');
+		$("#search").hide(); 
+	})
+	$(".close").click(function () {
+		$(".menu-item").removeClass('hide-item');
+		$(".header__search-form").removeClass('active');
+		$(".close").removeClass('active');
+		$("#search").show();
+	})
+
+	// sticky scroll header
+	window.addEventListener("scroll", function () {
+		const header = document.querySelector(".header__top-inner");
+		header.classList.toggle("sticky", window.scrollY > 0);
+	})
+})
